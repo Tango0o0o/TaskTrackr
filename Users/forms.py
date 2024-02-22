c1db98b71b759ae7aeb806019e6a50bfca6ca53b
Tango0o0o <dacostawalanyo49@gmail.com>
2024-02-22 13:13:27 +0000

    Created top navbar after 3 hours ffs

--- Users/forms.py
+++ Users/forms.py
@@ -5,9 +5,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 class Registration(UserCreationForm):
 
+    first_name = forms.CharField(max_length=50,label="First name")
+    last_name = forms.CharField(max_length=50,label="Last name")
+
     class Meta:
         model = User
-        fields = ["username", "email","password1","password2"]
+        fields = ["first_name", "last_name", "username", "email","password1"]
+    
+    
 
 class ProfileForm(forms.ModelForm):
 
